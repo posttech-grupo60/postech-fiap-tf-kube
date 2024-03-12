4fce03409ca473a825971e1f3066e78302c7266f
Bruno Grun <grunbruno@gmail.com>
2024-03-11 22:34:45 -0300

    add HPA and LoadBalancer

--- env/prod/main.tf
+++ env/prod/main.tf
@@ -4,3 +4,14 @@ module "prod" {
   cluster_name = "TechChallengeCluster"
 }
 
+output "LBName" {
+  value = module.prod.load_balancer_name
+}
+
+output "LBHostname" {
+  value = module.prod.load_balancer_hostname
+}
+
+output "LBInfo" {
+  value = module.prod.load_balancer_info
+}
